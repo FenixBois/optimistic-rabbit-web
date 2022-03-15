610421efee5678cbbbdf98c7456585191103eebe
Adam Tretera <adam.tretera@ackee.cz>
2022-03-15 21:20:46 +0100

    💄 Instaled windi for tailwind syntax

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const WindiCSSWebpackPlugin = require('windicss-webpack-plugin');
 
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+
+    webpack(config) {
+        config.plugins.push(new WindiCSSWebpackPlugin());
+        return config;
+    },
+};
+
+module.exports = nextConfig;
