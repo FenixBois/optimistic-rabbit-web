1e670f750e2a9f3f2aa975960dd26b7035295f50
Adam Tretera <adam.tretera@ackee.cz>
2022-05-16 19:15:41 +0200

    :sparkles: Added Select

--- src/components/Dashboard/SearchBarForm/SearchBarForm.tsx
+++ src/components/Dashboard/SearchBarForm/SearchBarForm.tsx
@@ -5,7 +5,7 @@ import { DROPDOWN_DATA } from './SearchBarFormData';
 import { SearchBarFormStyled, SearchInputBox, SelectContainerStyled } from './SearchBarForm.styles';
 
 export function SearchBarForm() {
-    const { handleSubmit, register, setValue } = useForm<{ bartok: string }>({ mode: 'onChange' });
+    const { handleSubmit, register, control } = useForm({ mode: 'onChange' });
 
     const onSubmit = (data: any) => {
         console.log(data);
@@ -25,11 +25,7 @@ export function SearchBarForm() {
                 </SearchInputBox>
                 <SelectContainerStyled>
                     {DROPDOWN_DATA.map(dropdown => (
-                        <Select
-                            key={dropdown.id}
-                            type={'primary'}
-                            onChange={(value: string) => setValue('bartok', value)}
-                        >
+                        <Select key={dropdown.id} type={'primary'} control={control} name={'barrtoksmradok'}>
                             {dropdown.options.map(option => (
                                 <SelectItem key={option.id} value={option.description}>
                                     {option.description}
--- src/components/UI/Select/Select.tsx
+++ src/components/UI/Select/Select.tsx
@@ -1,18 +1,18 @@
-import type {ReactNode} from 'react';
-import {useState} from 'react';
-import {Icon} from '../Icon';
+import type { ReactNode } from 'react';
+import { Icon } from '../Icon';
+import { Control, Controller } from 'react-hook-form';
 
-import {AnimatePresence, motion} from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
-import {ListboxButtonStyled, ListboxListStyled, SelectBody, SelectInput, SelectItemStyled} from './Select.styles';
-import {VariantProps} from '@stitches/react';
-import {subMenuAnimate} from "./SelectAnimation";
+import { ListboxButtonStyled, ListboxListStyled, SelectBody, SelectInput, SelectItemStyled } from './Select.styles';
+import { VariantProps } from '@stitches/react';
+import { subMenuAnimate } from './SelectAnimation';
 
 interface SelectTypeProps extends VariantProps<typeof SelectInput> {
     children: ReactNode;
-    onChange?: (value: string) => void;
-    
-
+    name: string;
+    control: Control;
+    defaultValue?: string;
 }
 
 interface SelectItemTypeProps extends VariantProps<typeof SelectItemStyled> {
@@ -20,62 +20,58 @@ interface SelectItemTypeProps extends VariantProps<typeof SelectItemStyled> {
     children: ReactNode;
 }
 
-export const Select = ({
-                           ref, children, type, onChange = () => {
-    }
-                       }: SelectTypeProps) => {
-    const [value, setValue] = useState<string | null>(null);
-
+export function Select({ control, children, defaultValue, type, name }: SelectTypeProps) {
     const SelectBodyAnimated = motion(SelectBody);
 
-    const handleChange = (value: string) => {
-        setValue(value);
-        onChange(value);
-    };
-
     return (
-
-        <SelectInput
-
-            type={type} value={value ?? 'default'} onChange={handleChange} ref={ref}
-        >
-            {({valueLabel, isExpanded}) => (
-                <>
-                    <ListboxButtonStyled
-                        arrow={
-                            isExpanded ? (
-                                <Icon
-                                    label={'close menu'}
-                                    size={type === 'primary' ? 'large' : 'medium'}
-                                    type={Icon.Types.ARROW_UP}
-                                />
-                            ) : (
-                                <Icon
-                                    label={'open menu'}
-                                    size={type === 'primary' ? 'large' : 'medium'}
-                                    type={Icon.Types.ARROW_DOWN}
-                                />
-                            )
-                        }
-                    >
-                        {value ? valueLabel : 'Difficulty'}
-                    </ListboxButtonStyled>
-                    <AnimatePresence exitBeforeEnter={false} initial={false} onExitComplete={() => null}>
-                        <SelectBodyAnimated
-                            initial="exit"
-                            type={type}
-                            animate={isExpanded ? 'enter' : 'exit'}
-                            variants={subMenuAnimate}
-                        >
-                            <ListboxListStyled>{children}</ListboxListStyled>
-                        </SelectBodyAnimated>
-                    </AnimatePresence>
-                </>
+        <Controller
+            control={control}
+            name={name}
+            render={({ field: { onChange, value, name, ref } }) => (
+                <SelectInput type={type} value={value} name={name} onChange={onChange} ref={ref}>
+                    {({ valueLabel, isExpanded }) => (
+                        <>
+                            <ListboxButtonStyled
+                                arrow={
+                                    isExpanded ? (
+                                        <Icon
+                                            label={'close menu'}
+                                            size={type === 'primary' ? 'large' : 'medium'}
+                                            type={Icon.Types.ARROW_UP}
+                                        />
+                                    ) : (
+                                        <Icon
+                                            label={'open menu'}
+                                            size={type === 'primary' ? 'large' : 'medium'}
+                                            type={Icon.Types.ARROW_DOWN}
+                                        />
+                                    )
+                                }
+                            >
+                                {value ? valueLabel : defaultValue}
+                            </ListboxButtonStyled>
+                            <AnimatePresence exitBeforeEnter={false} initial={false} onExitComplete={() => null}>
+                                <SelectBodyAnimated
+                                    initial="exit"
+                                    type={type}
+                                    animate={isExpanded ? 'enter' : 'exit'}
+                                    variants={subMenuAnimate}
+                                >
+                                    <ListboxListStyled>{children}</ListboxListStyled>
+                                </SelectBodyAnimated>
+                            </AnimatePresence>
+                        </>
+                    )}
+                </SelectInput>
             )}
-        </SelectInput>
+        />
     );
-};
+}
 
-export const SelectItem = ({children, value, type}: SelectItemTypeProps) => {
-    return <SelectItemStyled value={value} type={type}>{children}</SelectItemStyled>;
+export const SelectItem = ({ children, value, type }: SelectItemTypeProps) => {
+    return (
+        <SelectItemStyled value={value} type={type}>
+            {children}
+        </SelectItemStyled>
+    );
 };
